c64a43a9914121885a9eb2b4fd19549161372f58
mohamed.khater <m.khater@epicsyst.com>
2024-03-29 19:40:03 +0200

    Add Frameworks icons

---
+++ AppleFrameworks/Framework.swift
@@ -0,0 +1,8 @@
+//
+//  Framework.swift
+//  AppleFrameworks
+//
+//  Created by Epic_Sys on 29/03/2024.
+//
+
+import Foundation
---
+++ AppleFrameworks/MockData.swift
@@ -0,0 +1,8 @@
+//
+//  MockData.swift
+//  AppleFrameworks
+//
+//  Created by Epic_Sys on 29/03/2024.
+//
+
+import Foundation
